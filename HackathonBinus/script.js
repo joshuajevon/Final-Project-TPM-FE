8f532780d18fd893e84fdaed3bbb08ac587cd227
joshuajevon <joshuajevon05@gmail.com>
2022-01-15 22:15:27 +0700

    front end project

---
+++ HackathonBinus/script.js
@@ -0,0 +1,17 @@
+function showDesign(){
+    document.getElementById('lol1').style.display="grid";
+    document.getElementById('lol2').style.display="none";
+    document.getElementById('lol3').style.display="none";
+}
+
+function showTechnology(){
+    document.getElementById('lol1').style.display="none";
+    document.getElementById('lol2').style.display="grid";
+    document.getElementById('lol3').style.display="none";
+}
+
+function showBusiness(){
+    document.getElementById('lol1').style.display="none";
+    document.getElementById('lol2').style.display="none";
+    document.getElementById('lol3').style.display="grid";
+}
